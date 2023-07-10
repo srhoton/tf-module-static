2762dd41d38bdd434ecba4e398dfc4a584da3fbd
Stephen Rhoton <stephen.rhoton@dealerware.com>
2023-07-10 10:33:53 -0400

    Added tags to acm

--- acm.tf
+++ acm.tf
@@ -1,4 +1,5 @@
 resource "aws_acm_certificate" "cloudfront" {
   domain_name       = "${local.cloudfront_dns_name}.${var.route53_zone_name}"
   validation_method = "DNS"
+  tags = local.default_tags
 }
